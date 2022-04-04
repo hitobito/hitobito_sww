4bad869eb1bdf3cb390212ab7be9a223ebf3bfe7
Nils Rauch <rauch@puzzle.ch>
2022-04-04 12:41:00 +0200

    Wrap people import in transaction

--- lib/tasks/import.rake
+++ lib/tasks/import.rake
@@ -55,124 +55,126 @@ namespace :import do
       tag.merge!(id: ActsAsTaggableOn::Tag.find_by(name: tag[:name]).id)
     end
 
-    failed_import_rows = []
+    ActiveRecord::Base.transaction do
+      failed_import_rows = []
 
-    CSV.parse(person_csv.read, headers: true, header_converters: :symbol).each do |import_row|
-      person_attrs = DataMigrator.person_attrs_from_import_row(import_row)
+      CSV.parse(person_csv.read, headers: true, header_converters: :symbol).each do |import_row|
+        person_attrs = DataMigrator.person_attrs_from_import_row(import_row)
 
-      person_attrs[:magazin_abo_number] = import_row[:abo1number]
-      person_attrs[:title] = import_row[:title]
+        person_attrs[:magazin_abo_number] = import_row[:abo1number]
+        person_attrs[:title] = import_row[:title]
 
-      country_mappings = {
-        'Schweiz': 'CH',
-        'Deutschland': 'DE'
-      }
-
-      country = country_mappings[import_row[:primaryaddresscountrylictranslated]&.to_sym]
-      person_attrs[:country] = country || 'CH'
-      person_attrs[:name_add_on] = import_row[:nameaddon]
-
-      DataMigrator.assign_salutation!(person_attrs, import_row)
-
-      DataMigrator.assign_company!(person_attrs, import_row)
-
-      if person_attrs[:alabus_id]
-        Person.upsert(person_attrs)
-      else
-        failed_import_rows << import_row
-        next
-      end
-
-      person = Person.find_by(alabus_id: person_attrs[:alabus_id])
-
-      unless person
-        failed_import_rows << import_row
-        next
-      end
-
-      if import_row[:email].present? && person_attrs[:email].nil? # email is already taken
-        additional_mail_attrs = {
-          contactable_type: Person.sti_name,
-          contactable_id: person.id,
-          email: import_row[:email],
-          label: 'Privat'
+        country_mappings = {
+          'Schweiz': 'CH',
+          'Deutschland': 'DE'
         }
 
-        AdditionalEmail.upsert(additional_mail_attrs)
-      end
+        country = country_mappings[import_row[:primaryaddresscountrylictranslated]&.to_sym]
+        person_attrs[:country] = country || 'CH'
+        person_attrs[:name_add_on] = import_row[:nameaddon]
 
-      mitglied_attrs = {
-        person_id: person.id,
-        group_id: group.id,
-        type: Group::Mitglieder::Aktivmitglied.sti_name,
-        created_at: import_row[:memberentrydate],
-        deleted_at: import_row[:memberexitdate],
-        updated_at: Time.zone.now
-      }
+        DataMigrator.assign_salutation!(person_attrs, import_row)
 
-      magazin_abo_attrs = {
-        person_id: person.id,
-        group_id: group.id,
-        type: Group::Mitglieder::MagazinAbonnent.sti_name,
-        created_at: import_row[:abo1start],
-        deleted_at: import_row[:abo1end],
-        updated_at: Time.zone.now
-      }
+        DataMigrator.assign_company!(person_attrs, import_row)
 
-      [mitglied_attrs, magazin_abo_attrs].each do |attrs|
-        attrs[:created_at] = DateTime.parse(attrs[:created_at]) if attrs[:created_at]
-        attrs[:deleted_at] = DateTime.parse(attrs[:deleted_at]) if attrs[:deleted_at]
-
-        if attrs[:deleted_at].present? && attrs[:created_at].nil?
-          attrs[:created_at] = attrs[:deleted_at].yesterday
+        if person_attrs[:alabus_id]
+          Person.upsert(person_attrs)
+        else
+          failed_import_rows << import_row
+          next
         end
 
-        next unless attrs[:created_at]
+        person = Person.find_by(alabus_id: person_attrs[:alabus_id])
 
-        # Because of a known issue of the acts_as_paranoid gem,
-        # you can not directly create a model in a deleted state.
-        # Thus we have to update it afterwards.
-        Role.insert(attrs)
-        Role.last.update(attrs)
+        unless person
+          failed_import_rows << import_row
+          next
+        end
+
+        if import_row[:email].present? && person_attrs[:email].nil? # email is already taken
+          additional_mail_attrs = {
+            contactable_type: Person.sti_name,
+            contactable_id: person.id,
+            email: import_row[:email],
+            label: 'Privat'
+          }
+
+          AdditionalEmail.upsert(additional_mail_attrs)
+        end
+
+        mitglied_attrs = {
+          person_id: person.id,
+          group_id: group.id,
+          type: Group::Mitglieder::Aktivmitglied.sti_name,
+          created_at: import_row[:memberentrydate],
+          deleted_at: import_row[:memberexitdate],
+          updated_at: Time.zone.now
+        }
+
+        magazin_abo_attrs = {
+          person_id: person.id,
+          group_id: group.id,
+          type: Group::Mitglieder::MagazinAbonnent.sti_name,
+          created_at: import_row[:abo1start],
+          deleted_at: import_row[:abo1end],
+          updated_at: Time.zone.now
+        }
+
+        [mitglied_attrs, magazin_abo_attrs].each do |attrs|
+          attrs[:created_at] = DateTime.parse(attrs[:created_at]) if attrs[:created_at]
+          attrs[:deleted_at] = DateTime.parse(attrs[:deleted_at]) if attrs[:deleted_at]
+
+          if attrs[:deleted_at].present? && attrs[:created_at].nil?
+            attrs[:created_at] = attrs[:deleted_at].yesterday
+          end
+
+          next unless attrs[:created_at]
+
+          # Because of a known issue of the acts_as_paranoid gem,
+          # you can not directly create a model in a deleted state.
+          # Thus we have to update it afterwards.
+          Role.insert(attrs)
+          Role.last.update(attrs)
+        end
+
+        tagging_attrs = { taggable_id: person.id, taggable_type: Person.sti_name }
+
+        if import_row[:primarycategory].present?
+          tag_name = "category:#{import_row[:primarycategory]}"
+          tag = ActsAsTaggableOn::Tag.find_or_create_by!(name: tag_name)
+
+          ActsAsTaggableOn::Tagging.upsert(tagging_attrs.merge(tag_id: tag.id))
+        end
+
+        [:abo1, :primarycategory, :primarycommchannel].each do |tag|
+          tag_id = tag_mappings[import_row[tag]&.to_sym].try(:[], :id)
+
+          next unless tag_id
+
+          attrs = tagging_attrs.merge({ tag_id: tag_id })
+
+          ActsAsTaggableOn::Tagging.upsert(attrs)
+
+        end
+
+        DataMigrator.insert_phone_numbers!(person, import_row)
+        DataMigrator.insert_social_account!(person, import_row)
+        DataMigrator.insert_note!(person, import_row)
       end
 
-      tagging_attrs = { taggable_id: person.id, taggable_type: Person.sti_name }
-
-      if import_row[:primarycategory].present?
-        tag_name = "category:#{import_row[:primarycategory]}"
-        tag = ActsAsTaggableOn::Tag.find_or_create_by!(name: tag_name)
-
-        ActsAsTaggableOn::Tagging.upsert(tagging_attrs.merge(tag_id: tag.id))
+      total_count = CSV.parse(person_csv.read, headers: true).size
+      successful_count = total_count - failed_import_rows.size
+      puts "Successfully imported #{successful_count}/#{total_count} rows"
+      if failed_import_rows.any?
+        puts "FAILED ROWS:"
+        puts failed_import_rows.map { |row| ["first_name: #{row[:firstname]}",
+                                             "last_name: #{row[:lastname]}",
+                                             "email: #{row[:email]}",
+                                             "alabus_id: #{row[:id]}"].join(', ') }.join("\n")
+        raise ActiveRecord::Rollback
       end
 
-
-      [:abo1, :primarycommchannel].each do |tag|
-        tag_id = tag_mappings[import_row[tag]&.to_sym].try(:[], :id)
-
-        next unless tag_id
-
-        attrs = tagging_attrs.merge({ tag_id: tag_id })
-
-        ActsAsTaggableOn::Tagging.upsert(attrs)
-
-      end
-
-      DataMigrator.insert_phone_numbers!(person, import_row)
-      DataMigrator.insert_social_account!(person, import_row)
-      DataMigrator.insert_note!(person, import_row)
     end
-
-    total_count = CSV.parse(person_csv.read, headers: true).size
-    successful_count = total_count - failed_import_rows.size
-    puts "Successfully imported #{successful_count}/#{total_count} rows"
-    if failed_import_rows.any?
-      puts "FAILED ROWS:"
-      puts failed_import_rows.map { |row| ["first_name: #{row[:firstname]}",
-                                           "last_name: #{row[:lastname]}",
-                                           "email: #{row[:email]}",
-                                           "alabus_id: #{row[:id]}"].join(', ') }.join("\n")
-    end
-
   end
 
   desc 'Import invoices for FO'
--- spec/tasks/import_spec.rb
+++ spec/tasks/import_spec.rb
@@ -3,156 +3,167 @@ require 'spec_helper'
 Rails.application.load_tasks
 
 describe "import:people_fo" do
-  before do
-    expect_any_instance_of(Pathname).to receive(:join)
-                                    .and_return(Wagons.find('sww')
-                                                      .root
-                                                      .join('spec/fixtures/files/people_fo.csv'))
-  end
-
-  after do
-    Rake::Task["import:people_fo"].reenable
-  end
-
-  it "raises if given no argument" do
-    expect do
-      Rake::Task["import:people_fo"].invoke
-    end.to raise_error(RuntimeError, 'group id must be passed as first argument')
-  end
-
-  it "raises if group with given id does not exist" do
-    expect do
-      Rake::Task["import:people_fo"].invoke(Group.maximum(:id).succ)
-    end.to raise_error(ActiveRecord::RecordNotFound)
-  end
-
-  it "raises if group with given id does not exist" do
-    expect do
-      Rake::Task["import:people_fo"].invoke(Group.maximum(:id).succ)
-    end.to raise_error(ActiveRecord::RecordNotFound)
-  end
-
-  it "imports people and companies from csv" do
-    expect do
-      Rake::Task["import:people_fo"].invoke(groups(:berner_wanderwege).id)
-    end.to change { Person.count }.by(5)
-
-    person = Person.find_by(alabus_id: '1skuw-b52nqz-g2iw4kjn-2-g21sdwqh-qa7')
-    expect(person).to be_present
-
-    expect(person.country).to eq('DE')
-    expect(person.title).to eq('Dr.')
-    expect(person.gender).to eq('m')
-    expect(person.magazin_abo_number).to eq(1000)
-    expect(person.name_add_on).to eq('Mustermann')
-    expect(person.email).to eq('max.muster@example.com')
-    expect(person.roles.with_deleted.count).to eq(2)
-
-    mitglied = person.roles.first
-    magazin_abo = person.roles.with_deleted.last
-
-    expect(mitglied.type).to eq(Group::Mitglieder::Aktivmitglied.sti_name)
-    expect(mitglied.created_at).to eq(DateTime.new(1977, 1, 1))
-    expect(mitglied.deleted_at).to be_nil
-
-    expect(magazin_abo.type).to eq(Group::Mitglieder::MagazinAbonnent.sti_name)
-    expect(magazin_abo.created_at).to eq(DateTime.new(1990, 10, 12))
-    expect(magazin_abo.deleted_at).to eq(DateTime.new(2006, 2, 12))
-
-    expect(person.taggings.count).to eq(3)
-
-    person.taggings.each do |tagging|
-      expect(['abo:kombi', 'category:Einzelmitglied mit Magazin', 'Newsletter']).to include(tagging.tag.name)
+  context 'valid people' do
+    before do
+      allow_any_instance_of(Pathname).to receive(:join)
+        .and_return(Wagons.find('sww')
+        .root
+        .join('spec/fixtures/files/people_fo.csv'))
     end
 
-    expect(person.phone_numbers.count).to eq(2)
+    after do
+      Rake::Task["import:people_fo"].reenable
+    end
 
-    mobile = person.phone_numbers.find_by(label: 'Mobil')
-    expect(mobile.number).to eq('+41 12 300 30 30')
+    it "raises if given no argument" do
+      expect do
+        Rake::Task["import:people_fo"].invoke
+      end.to raise_error(RuntimeError, 'group id must be passed as first argument')
+    end
 
-    main = person.phone_numbers.find_by(label: 'Privat')
-    expect(main.number).to eq('+41 42 300 30 30')
+    it "raises if group with given id does not exist" do
+      expect do
+        Rake::Task["import:people_fo"].invoke(Group.maximum(:id).succ)
+      end.to raise_error(ActiveRecord::RecordNotFound)
+    end
 
-    expect(person.social_accounts.first.name).to eq('https://www.hitobito.com')
+    it "raises if group with given id does not exist" do
+      expect do
+        Rake::Task["import:people_fo"].invoke(Group.maximum(:id).succ)
+      end.to raise_error(ActiveRecord::RecordNotFound)
+    end
 
-    expect(person.notes.first.text).to eq('GV')
+    it "imports people and companies from csv" do
+      expect do
+        Rake::Task["import:people_fo"].invoke(groups(:berner_wanderwege).id)
+      end.to change { Person.count }.by(5)
 
-    company = Person.find_by(alabus_id: 'haw31-axzcd1-jb44x23z-z-jtxn23wd1-k42')
-    expect(company).to be_present
-    expect(company.company_name).to eq('Hitobito AG')
-    expect(company.member_number).to eq(42)
-  end
+      person = Person.find_by(alabus_id: '1skuw-b52nqz-g2iw4kjn-2-g21sdwqh-qa7')
+      expect(person).to be_present
 
-  it "imports mail as additional mail if already taken" do
-    Rake::Task["import:people_fo"].invoke(groups(:berner_wanderwege).id)
+      expect(person.country).to eq('DE')
+      expect(person.title).to eq('Dr.')
+      expect(person.gender).to eq('m')
+      expect(person.magazin_abo_number).to eq(1000)
+      expect(person.name_add_on).to eq('Mustermann')
+      expect(person.email).to eq('max.muster@example.com')
+      expect(person.roles.with_deleted.count).to eq(2)
 
-    person = Person.find_by(alabus_id: '1s23w-b52n1x-2ciw2kjn-g-g213bwvh-1x7')
-    expect(person).to be_present
+      mitglied = person.roles.first
+      magazin_abo = person.roles.with_deleted.last
 
-    expect(person.email).to be_nil
-    expect(person.additional_emails.count).to eq(1)
-    expect(person.additional_emails.first.email).to eq('max.muster@example.com')
-  end
+      expect(mitglied.type).to eq(Group::Mitglieder::Aktivmitglied.sti_name)
+      expect(mitglied.created_at).to eq(DateTime.new(1977, 1, 1))
+      expect(mitglied.deleted_at).to be_nil
 
-  it "sets role created_at on a day before deleted_at if not set" do
-    Rake::Task["import:people_fo"].invoke(groups(:berner_wanderwege).id)
+      expect(magazin_abo.type).to eq(Group::Mitglieder::MagazinAbonnent.sti_name)
+      expect(magazin_abo.created_at).to eq(DateTime.new(1990, 10, 12))
+      expect(magazin_abo.deleted_at).to eq(DateTime.new(2006, 2, 12))
 
-    person = Person.find_by(alabus_id: 'dcwe1-vbsdw2-2cib1kbs-p-g2bnbw1h-2sd')
-    expect(person).to be_present
+      expect(person.taggings.count).to eq(3)
 
-    expect(person.roles.without_deleted.count).to eq(0)
-    expect(person.roles.with_deleted.count).to eq(2)
+      person.taggings.each do |tagging|
+        expect(['abo:kombi', 'category:Einzelmitglied mit Magazin', 'Newsletter']).to include(tagging.tag.name)
+      end
 
-    mitglied = person.roles.with_deleted.first
-    magazin_abo = person.roles.with_deleted.last
+      expect(person.phone_numbers.count).to eq(2)
 
-    expect(mitglied.type).to eq(Group::Mitglieder::Aktivmitglied.sti_name)
-    expect(mitglied.created_at).to eq(DateTime.new(2002, 11, 29))
-    expect(mitglied.deleted_at).to eq(DateTime.new(2002, 11, 30))
+      mobile = person.phone_numbers.find_by(label: 'Mobil')
+      expect(mobile.number).to eq('+41 12 300 30 30')
 
-    expect(magazin_abo.type).to eq(Group::Mitglieder::MagazinAbonnent.sti_name)
-    expect(magazin_abo.created_at).to eq(DateTime.new(1998, 12, 31))
-    expect(magazin_abo.deleted_at).to eq(DateTime.new(1999, 1, 1))
-  end
+      main = person.phone_numbers.find_by(label: 'Privat')
+      expect(main.number).to eq('+41 42 300 30 30')
 
-  it "imports role only if created_at can be set" do
-    Rake::Task["import:people_fo"].invoke(groups(:berner_wanderwege).id)
+      expect(person.social_accounts.first.name).to eq('https://www.hitobito.com')
 
-    person_with_two_roles = Person.find_by(alabus_id: '1skuw-b52nqz-g2iw4kjn-2-g21sdwqh-qa7')
-    person_with_one_role = Person.find_by(alabus_id: '1s23w-b52n1x-2ciw2kjn-g-g213bwvh-1x7')
-    person_without_roles = Person.find_by(alabus_id: 'bew31-axzcd1-jbhox23z-z-jtxn23wd1-k3g')
+      expect(person.notes.first.text).to eq('GV')
 
-    expect(person_with_two_roles.roles.with_deleted.count).to eq(2)
-    expect(person_with_one_role.roles.with_deleted.count).to eq(1)
-    expect(person_without_roles.roles.with_deleted.count).to eq(0)
-  end
+      company = Person.find_by(alabus_id: 'haw31-axzcd1-jb44x23z-z-jtxn23wd1-k42')
+      expect(company).to be_present
+      expect(company.company_name).to eq('Hitobito AG')
+      expect(company.member_number).to eq(42)
+    end
 
-  it "assigns Schweiz as fallback country" do
-    Rake::Task["import:people_fo"].invoke(groups(:berner_wanderwege).id)
-
-    person = Person.find_by(alabus_id: 'bew31-axzcd1-jbhox23z-z-jtxn23wd1-k3g')
-    expect(person).to be_present
-
-    expect(person.country).to eq('CH')
-  end
-
-  it "assigns Schweiz as fallback country" do
-    Rake::Task["import:people_fo"].invoke(groups(:berner_wanderwege).id)
-
-    person = Person.find_by(alabus_id: 'bew31-axzcd1-jbhox23z-z-jtxn23wd1-k3g')
-    expect(person).to be_present
-
-    expect(person.country).to eq('CH')
-  end
-
-  it "does not import person without alabus id and prints to stdout" do
-    expected_output = ['Successfully imported 5/6 rows',
-                       'FAILED ROWS:',
-                       "first_name: Daniel, last_name: Failing, email: failing@example.com, alabus_id: \n"].join("\n")
-
-    expect do
+    it "imports mail as additional mail if already taken" do
       Rake::Task["import:people_fo"].invoke(groups(:berner_wanderwege).id)
-    end.to output(expected_output).to_stdout
+
+      person = Person.find_by(alabus_id: '1s23w-b52n1x-2ciw2kjn-g-g213bwvh-1x7')
+      expect(person).to be_present
+
+      expect(person.email).to be_nil
+      expect(person.additional_emails.count).to eq(1)
+      expect(person.additional_emails.first.email).to eq('max.muster@example.com')
+    end
+
+    it "sets role created_at on a day before deleted_at if not set" do
+      Rake::Task["import:people_fo"].invoke(groups(:berner_wanderwege).id)
+
+      person = Person.find_by(alabus_id: 'dcwe1-vbsdw2-2cib1kbs-p-g2bnbw1h-2sd')
+      expect(person).to be_present
+
+      expect(person.roles.without_deleted.count).to eq(0)
+      expect(person.roles.with_deleted.count).to eq(2)
+
+      mitglied = person.roles.with_deleted.first
+      magazin_abo = person.roles.with_deleted.last
+
+      expect(mitglied.type).to eq(Group::Mitglieder::Aktivmitglied.sti_name)
+      expect(mitglied.created_at).to eq(DateTime.new(2002, 11, 29))
+      expect(mitglied.deleted_at).to eq(DateTime.new(2002, 11, 30))
+
+      expect(magazin_abo.type).to eq(Group::Mitglieder::MagazinAbonnent.sti_name)
+      expect(magazin_abo.created_at).to eq(DateTime.new(1998, 12, 31))
+      expect(magazin_abo.deleted_at).to eq(DateTime.new(1999, 1, 1))
+    end
+
+    it "imports role only if created_at can be set" do
+      Rake::Task["import:people_fo"].invoke(groups(:berner_wanderwege).id)
+
+      person_with_two_roles = Person.find_by(alabus_id: '1skuw-b52nqz-g2iw4kjn-2-g21sdwqh-qa7')
+      person_with_one_role = Person.find_by(alabus_id: '1s23w-b52n1x-2ciw2kjn-g-g213bwvh-1x7')
+      person_without_roles = Person.find_by(alabus_id: 'bew31-axzcd1-jbhox23z-z-jtxn23wd1-k3g')
+
+      expect(person_with_two_roles.roles.with_deleted.count).to eq(2)
+      expect(person_with_one_role.roles.with_deleted.count).to eq(1)
+      expect(person_without_roles.roles.with_deleted.count).to eq(0)
+    end
+
+    it "assigns Schweiz as fallback country" do
+      Rake::Task["import:people_fo"].invoke(groups(:berner_wanderwege).id)
+
+      person = Person.find_by(alabus_id: 'bew31-axzcd1-jbhox23z-z-jtxn23wd1-k3g')
+      expect(person).to be_present
+
+      expect(person.country).to eq('CH')
+    end
+
+    it "assigns Schweiz as fallback country" do
+      Rake::Task["import:people_fo"].invoke(groups(:berner_wanderwege).id)
+
+      person = Person.find_by(alabus_id: 'bew31-axzcd1-jbhox23z-z-jtxn23wd1-k3g')
+      expect(person).to be_present
+
+      expect(person.country).to eq('CH')
+    end
+  end
+
+  context 'invalid people' do
+    before do
+      allow_any_instance_of(Pathname).to receive(:join)
+        .and_return(Wagons.find('sww')
+        .root
+        .join('spec/fixtures/files/invalid_people_fo.csv'))
+    end
+
+    it "does not import person without alabus id and prints to stdout" do
+      expected_output = ['Successfully imported 5/6 rows',
+                         'FAILED ROWS:',
+                         "first_name: Daniel, last_name: Failing, email: failing@example.com, alabus_id: \n"].join("\n")
+
+      expect do
+        Rake::Task["import:people_fo"].invoke(groups(:berner_wanderwege).id)
+      end.to output(expected_output).to_stdout
+    end
   end
 end
 
