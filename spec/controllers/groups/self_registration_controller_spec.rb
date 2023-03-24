1efdbde47d4a78f18ee133bf18e2b54fa0603b1c
Nils Rauch <nils.rauch@gmail.com>
2023-03-24 09:14:13 +0100

    Adjust self registration forms

---
+++ spec/controllers/groups/self_registration_controller_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2023, Schweizer Wanderwege. This file is part of
+#  hitobito_sww and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sww.
+
+require 'spec_helper'
+
+describe Groups::SelfRegistrationController do
+  let(:group) { groups(:berner_mitglieder) }
+
+  before do
+    group.update!(self_registration_role_type: Group::Mitglieder::Aktivmitglied.sti_name)
+  end
+
+  context 'POST create' do
+    it 'builds address using street and house_number' do
+      expect do
+        post :create, params: {
+          group_id: group.id,
+          role: {
+            group_id: group.id,
+            new_person: { first_name: 'Bob',
+                          last_name: 'Miller', 
+                          email: 'bob.miller@example.com',
+                          street: 'Belpstrasse',
+                          house_number: '37' }
+          }
+        }
+      end.to change { Person.count }.by(1)
+
+      person = Person.find_by(email: 'bob.miller@example.com')
+
+      expect(person.address).to eq('Belpstrasse 37')
+    end
+  end
+end
