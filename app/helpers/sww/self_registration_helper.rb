eb3d3dea70d1add96b48ea3d8c7ce0f8d64ee406
Mountain Star <github@mtnstar.net>
2023-03-28 11:07:55 +0200

    Add sww self registration conditions

---
+++ app/helpers/sww/self_registration_helper.rb
@@ -0,0 +1,24 @@
+#  frozen_string_literal: true
+
+#  Copyright (c) 2023, Schweizer Wanderwege. This file is part of
+#  hitobito_sww and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sww.
+
+module Sww::SelfRegistrationHelper
+
+  def link_to_sww_privacy
+    link_to(
+      t('.privacy_link_name'),
+      t('.privacy_link'),
+      target: '_blank')
+  end
+
+  def link_to_sww_terms_of_use
+    link_to(
+      t('.terms_of_use_link_name'),
+      t('.terms_of_use_link'),
+      target: '_blank')
+  end
+
+end
