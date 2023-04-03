29000d819bcc3d4fdecceca69fd3dd0b10e05cd5
Nils Rauch <rauch@puzzle.ch>
2023-04-03 15:55:58 +0200

    Mark Benutzerkonto role as basic_permissions_only

---
+++ spec/abilities/person_ability_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2023, Schweizer Wanderwege. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sww.
+
+require 'spec_helper'
+
+describe PersonAbility do
+
+  subject { ability }
+  let(:ability) { Ability.new(role.person.reload) }
+
+  context 'person with only benutzerkonto role' do
+    let(:role) do
+      Fabricate(Group::Benutzerkonten::Benutzerkonto.sti_name.to_sym,
+                group: groups(:benutzerkonten))
+    end
+
+    it 'may not show_details herself' do
+      is_expected.to_not be_able_to(:show_details, role.person)
+    end
+
+    it 'may not show_full herself' do
+      is_expected.to_not be_able_to(:show_full, role.person)
+    end
+
+    it 'may not show history on herself' do
+      is_expected.to_not be_able_to(:history, role.person)
+    end
+
+    it 'may not show log on herself' do
+      is_expected.to_not be_able_to(:log, role.person)
+    end
+
+    it 'may not index invoices on herself' do
+      is_expected.to_not be_able_to(:index_invoices, role.person)
+    end
+
+  end
+end
