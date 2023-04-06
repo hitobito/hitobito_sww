c6acf4fc9a14b8c0e678fde86664d4dbec5ab9cd
Mountain Star <github@mtnstar.net>
2023-04-06 09:55:50 +0200

    Update fix duplicate script

--- lib/import_fix/fix_duplicate_cms_profile_id.rb
+++ lib/import_fix/fix_duplicate_cms_profile_id.rb
@@ -1,3 +1,5 @@
+@duplicates_with_no_email_person_ids = []
+
 def duplicate_entries
   columns_that_make_record_distinct = [:sww_cms_profile_id]
   distinct_ids = Person.select("MIN(id) as id").group(columns_that_make_record_distinct).map(&:id)
@@ -6,6 +8,18 @@ end
 
 def compare_person_entries(cms_profile_id)
   duplicates = Person.where(sww_cms_profile_id: cms_profile_id)
+
+  # check if one email present
+  unless duplicates.one?{|p| p.email.present?}
+    @duplicates_with_no_email_person_ids.push(duplicates.collect(&:id).join(','))
+    return
+  end
+
+  duplicates.each do |p|
+    if p.email.nil?
+      p.destroy!
+    end
+  end
 end
 
 @duplicate_entries = duplicate_entries
@@ -16,3 +30,5 @@ duplicate_profile_ids = @duplicate_entries.collect{|p| p.sww_cms_profile_id}.uni
 duplicate_profile_ids.each do |id|
   compare_person_entries(id)
 end
+
+puts "The following duplicates do not have an email: #{@duplicates_with_no_email_person_ids}"
