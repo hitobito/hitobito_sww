dc7bcd3c375fd00cb06549f2c1e9705bb011453d
Nils Rauch <nils.rauch@gmail.com>
2023-04-06 17:30:04 +0200

    Show sww_cms_profile_ids missing in DB

--- lib/import_fix/stats_import_csv.rb
+++ lib/import_fix/stats_import_csv.rb
@@ -7,13 +7,21 @@ def valid_pw_hashes?(row)
   hash.present? && hash.start_with?('$2y$', '$2a$') && salt.present?
 end
 
-all_people_cms = CSV.parse(File.read("/tmp/all_people_cms.csv"), headers: true, col_sep: ';', header_converters: :symbol)
+people_csv = Wagons.find('sww').root.join('tmp/people_cms.csv')
+all_people_cms = CSV.parse(people_csv.read, headers: true, col_sep: ';', header_converters: :symbol)
 puts "Number of entries in total: #{all_people_cms.count}"
 
 # exist in hitobito by cms_profile_id
 all_cms_profile_ids = all_people_cms.collect{|r| r[:profile_id]}.compact
 hitobito_cms_profile_id_people = Person.where(sww_cms_profile_id: all_cms_profile_ids)
 puts "Number of entries exist in hitobito by cms_profile_id: #{hitobito_cms_profile_id_people.count}"
+unique_hitobito_cms_profile_ids = hitobito_cms_profile_id_people.group(:sww_cms_profile_id).count.keys
+non_existing_cms_profile_ids_in_hitobito = all_cms_profile_ids.map(&:to_i) - unique_hitobito_cms_profile_ids
+if non_existing_cms_profile_ids_in_hitobito.size.nonzero?
+  puts "sww_cms_profile_id's present in CSV but not in hitobito (#{non_existing_cms_profile_ids_in_hitobito.size}/#{all_cms_profile_ids.size}):"
+  puts non_existing_cms_profile_ids_in_hitobito.join(",")
+end
+
 
 # valid e-mail addresses
 valid_email_entries = all_people_cms.select{|r| Truemail.valid?("#{r[:profile_email]}", with: :regex)}
