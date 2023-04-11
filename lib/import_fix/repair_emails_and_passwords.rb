e8056d1883d5b414967bbff1a6ff9af59dd1063c
Nils Rauch <rauch@puzzle.ch>
2023-04-11 08:30:57 +0200

    Iterate through new and existing people separately

--- lib/import_fix/repair_emails_and_passwords.rb
+++ lib/import_fix/repair_emails_and_passwords.rb
@@ -5,7 +5,11 @@ ActiveRecord::Base.logger = nil
 # TODO SWW CMS PROFILE ID 42918, 49308 in CSV hat keinen Namen, löschen oder anders bereinigen
 # TODO Scheinbar ist beim ersten Import eine fehlerhafte PLZ reingerutscht und bei den Callbacks geht jetzt aufgrund alles kaputt: Person.find(79555).update_column(:zip_code, nil)
 
-all_people_cms = CSV.parse(File.read("/tmp/all_people_cms.csv"), headers: true, col_sep: ';', header_converters: :symbol)
+@all_people_cms = CSV.parse(File.read("/tmp/all_people_cms.csv"), headers: true, col_sep: ';', header_converters: :symbol)
+
+def row_by_cms_id(cms_profile_id)
+  @all_people_cms.find { |r| r[:profile_id].to_i == cms_profile_id.to_i }
+end
 
 missing_person_creation_errors = []
 taken_email_for_new_person_errors = []
@@ -13,79 +17,90 @@ invalid_zip_people = []
 invalid_country_people = []
 invalid_language_people = []
 
-all_people_cms.each do |row|
-  person = Person.find_by(sww_cms_profile_id: row[:profile_id])
-  if person.present?
-    if person.email.nil? && row[:profile_email].present? && Truemail.valid?(row[:profile_email])
-      puts "Setting mail #{row[:profile_email]} for hitobito id: #{person.id}"
-      person.email = row[:profile_email]
-    end
+all_cms_profile_ids = @all_people_cms.collect{|r| r[:profile_id]}.compact
+hitobito_cms_profile_id_people = Person.where(sww_cms_profile_id: all_cms_profile_ids)
+non_existing_cms_profile_ids_in_hitobito = all_cms_profile_ids.map(&:to_i) - hitobito_cms_profile_id_people.pluck(:sww_cms_profile_id)
 
-    unless person.encrypted_password.present?
-      password = DataMigratorCms.set_password!({}, row)
-      if password.present?
-        puts "Setting password for hitobito id: #{person.id}"
-        person.encrypted_password = password
-      end
-    end
+hitobito_cms_profile_id_people.find_each do |person|
+  row = row_by_cms_id(person.sww_cms_profile_id)
 
-    unless person.valid?
-      if person.errors.errors.map(&:attribute).include?(:country)
-        person.country = nil
-        invalid_country_people << person.id
-      end
-      person.validate
-      if person.errors.errors.map(&:attribute).include?(:zip_code)
-        person.zip_code = nil
-        invalid_zip_people << person.id
-      end
-      if person.errors.errors.map(&:attribute).include?(:language)
-        person.language = :de
-        invalid_language_people << person.id
-      end
-    end
+  if person.email.nil? && row[:profile_email].present? && Truemail.valid?(row[:profile_email])
+    puts "Setting mail #{row[:profile_email]} for hitobito id: #{person.id}"
+    person.email = row[:profile_email]
 
-    if person.changed?
-      person.confirm
-      person.save!
-    end
-  else
-    person_attrs = DataMigratorCms.person_attrs_from_import_row(row)
-
-    if Person.exists?(email: person_attrs[:email])
-      mail = person_attrs.delete(:email)
-      taken_email_for_new_person_errors << "Person with cms_profile_id #{row[:profile_id]} could not set email #{mail} due to it already existing"
-    end
-
-    person_attrs[:language] = :de
-    person_attrs[:country] = :CH
-
-    person_attrs[:primary_group_id] = DataMigratorCms.default_user_group_id
-
-    DataMigratorCms.assign_company!(person_attrs, row)
-
-    unless Person.exists?(email: person_attrs[:email])
-      DataMigratorCms.set_password!(person_attrs, row)
-    end
-
-    person_attrs.transform_values! do |v|
-      if v.is_a?(String)
-        v.strip!
-      end
-      v
-    end
-
-    puts "Creating new hitobito person for cms_profile_id: #{row[:profile_id]}"
-    person = Person.new(person_attrs)
-    unless person.valid?
-      missing_person_creation_errors << "cms_profile_id #{row[:profile_id]}: #{person.errors.errors.map(&:attribute).join(',')}"
-      next
-    end
     person.confirm
-    person.save!
-
-    DataMigratorCms.insert_role!(person)
+    person.skip_reconfirmation!
+    person.skip_confirmation!
   end
+
+  unless person.encrypted_password.present?
+    password = DataMigratorCms.set_password!({}, row)
+    if password.present?
+      puts "Setting password for hitobito id: #{person.id}"
+      person.encrypted_password = password
+    end
+  end
+
+  unless person.valid?
+    if person.errors.errors.map(&:attribute).include?(:country)
+      person.country = nil
+      invalid_country_people << person.id
+    end
+    person.validate
+    if person.errors.errors.map(&:attribute).include?(:zip_code)
+      person.zip_code = nil
+      invalid_zip_people << person.id
+    end
+    if person.errors.errors.map(&:attribute).include?(:language)
+      person.language = :de
+      invalid_language_people << person.id
+    end
+  end
+
+  if person.changed?
+    person.save!
+  end
+end
+
+non_existing_cms_profile_ids_in_hitobito.each do |cms_profile_id|
+  row = row_by_cms_id(cms_profile_id)
+
+  person_attrs = DataMigratorCms.person_attrs_from_import_row(row)
+
+  if Person.exists?(email: person_attrs[:email])
+    mail = person_attrs.delete(:email)
+    taken_email_for_new_person_errors << "Person with cms_profile_id #{row[:profile_id]} could not set email #{mail} due to it already existing"
+  end
+
+  person_attrs[:language] = :de
+  person_attrs[:country] = :CH
+
+  person_attrs[:primary_group_id] = DataMigratorCms.default_user_group_id
+
+  DataMigratorCms.assign_company!(person_attrs, row)
+
+  unless Person.exists?(email: person_attrs[:email])
+    DataMigratorCms.set_password!(person_attrs, row)
+  end
+
+  person_attrs.transform_values! do |v|
+    if v.is_a?(String)
+      v.strip!
+    end
+    v
+  end
+
+  puts "Creating new hitobito person for cms_profile_id: #{row[:profile_id]}"
+  person = Person.new(person_attrs)
+  unless person.valid?
+    missing_person_creation_errors << "cms_profile_id #{row[:profile_id]}: #{person.errors.errors.map(&:attribute).join(',')}"
+    next
+  end
+  person.confirm
+  person.skip_confirmation!
+  person.save!
+
+  DataMigratorCms.insert_role!(person)
 end
 
 puts "FAILED DURING CREATION:"
