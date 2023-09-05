0be9c26cc52b57db4d6ac57f42bfcb947784d417
Nils Rauch <rauch@puzzle.ch>
2023-09-05 07:54:04 +0200

    Remove sww customization

--- app/domain/sww/export/pdf/invoice/articles.rb
+++ app/domain/sww/export/pdf/invoice/articles.rb
@@ -62,33 +62,6 @@ module Sww::Export::Pdf::Invoice::Articles
     ]
   end
 
-  def total_box # rubocop:disable Metrics/MethodLength
-    bounding_box([0, cursor], width: bounds.width) do
-      font_size(8) do
-        pdf.table total_data, position: :right, cell_style: { borders: [],
-                                                              border_color: 'CCCCCC',
-                                                              border_width: 0.5 } do
-          rows(0..1).padding = [2, 0]
-
-          row(1).font_style = :bold
-          row(1).borders = [:bottom, :top]
-          row(1).padding = [5, 0]
-          row(1).column(0).padding = [5, 15, 5, 0]
-
-          column(1).align = :right
-        end
-      end
-    end
-  end
-
-  def total_data
-    decorated = invoice.decorate
-    [
-      [I18n.t('invoices.pdf.cost'), decorated.cost],
-      [I18n.t('invoices.pdf.total'), decorated.total]
-    ]
-  end
-
   private
 
   def articles
