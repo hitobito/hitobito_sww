90c278aa2382cf98c6705505fa1f134b7ef48803
Matthias Viehweger <viehweger@puzzle.ch>
2024-01-25 11:42:17 +0100

    Use custom font in all PDFs

---
+++ app/domain/sww/export/pdf/participation/runner.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2024, Schweizer Wanderwege. This file is part of
+#  hitobito_sww and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sww.
+
+module Sww::Export::Pdf::Participation::Runner
+  private
+
+  def customize(pdf)
+    Sww::Export::Pdf::Font
+      .new(super(pdf))
+      .customize
+  end
+end
