bb8be880859a38b7935407ce868561fa90d38e58
Matthias Viehweger <viehweger@puzzle.ch>
2024-11-18 22:04:41 +0100

    Reload Invoice and InvoiceConfig from schema

---
+++ spec/support/reload_column_information.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2024, Puzzle ITC. This file is part of
+#  hitobito_sww and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sww
+
+[
+  Invoice,
+  InvoiceConfig
+].map(&:reset_column_information)
