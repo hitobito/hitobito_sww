e243ac941e8d292a4b57b5311b49d9b5c76b4672
Nils Rauch <rauch@puzzle.ch>
2025-03-24 13:20:51 +0100

    Adjust to core change

--- app/domain/sww/export/pdf/invoice/articles.rb
+++ app/domain/sww/export/pdf/invoice/articles.rb
@@ -9,9 +9,10 @@ module Sww::Export::Pdf::Invoice::Articles
   def render # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
     move_cursor_to 510
     pdf.move_down 1.cm
-    reminder = invoice.payment_reminders.last
-    font_size(12) { text title(reminder) }
-    render_reminder_text(reminder) if @options[:reminders]
+    font_size(12) { text title }
+    pdf.move_down 8
+    render_description if render_description?
+    render_reminder if render_reminder?
     pdf.move_down 10
     pdf.font_size(8) { articles_table }
 
