eb9b40273d460145e4f0f6b7a4312e24432761bb
Philipp Koster <koster@puzzle.ch>
2025-07-09 14:19:53 +0200

    Update license information

--- db/migrate/20230119132734_migrate_to_generated_member_number_2.rb
+++ db/migrate/20230119132734_migrate_to_generated_member_number_2.rb
@@ -1,9 +1,10 @@
-#  frozen_string_literal: true
+# frozen_string_literal: true
 
-#  Copyright (c) 2012-2023, Schweizer Wanderwege. This file is part of
+#  Copyright (c) 2012-2025, Schweizer Wanderwege. This file is part of
 #  hitobito_sww and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_sww.
+
 
 # This migration does not make any schema changes, it just transforms the data.
 # If this migration breaks at a point far in the future, just replace it with a noop migration
--- db/migrate/20231129141846_change_group_address_positions_type.rb
+++ db/migrate/20231129141846_change_group_address_positions_type.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2012-2025, Schweizer Wanderwege. This file is part of
+#  hitobito_sww and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sww.
+
+
 class ChangeGroupAddressPositionsType < ActiveRecord::Migration[6.1]
   def change
     # These address position columns have been created as integer columns by mistake.
