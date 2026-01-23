d3291a0b8a5d9915b4dae9684dc25c78114b8c8f
Daniel Illi <daniel@illi.zone>
2026-01-23 18:06:14 +0100

    Render member_number on invoice (#281)

--- app/domain/sww/export/pdf/invoice/invoice_information.rb
+++ app/domain/sww/export/pdf/invoice/invoice_information.rb
@@ -7,14 +7,23 @@
 
 module Sww::Export::Pdf::Invoice::InvoiceInformation
   def render
-    return if invoice.issued_at.blank?
-
     bounding_box([303, 480], width: 180, height: 20) do
-      text information, align: :right
+      text contents.shift, align: :right if contents.any?
+    end
+
+    bounding_box([303, 505], width: 180, height: 20) do
+      text contents.shift, align: :right, valign: :bottom if contents.any?
     end
   end
 
   private
+
+  def contents
+    @contents ||= [].tap do |items|
+      items << information if invoice.issued_at.present?
+      items << member_number if member_number.present?
+    end
+  end
 
   def information
     if @options[:reminders] && invoice.payment_reminders.exists?
@@ -27,5 +36,11 @@ module Sww::Export::Pdf::Invoice::InvoiceInformation
 
   def group
     invoice.group
+  end
+
+  def member_number
+    invoice.recipient.try(:member_number).then do |number|
+      "#{Person.human_attribute_name(:member_number)}: #{number}" if number.present?
+    end
   end
 end
