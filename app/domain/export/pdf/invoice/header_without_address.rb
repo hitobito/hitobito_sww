8737b3fc54ab005138931fc2f7d3ffd9ca3009b7
carlobeltrame <carlo@beltra.me>
2026-01-23 13:53:56 +0100

    Don't render sender address when printing membership_card (hitobito/hitobito_ejv#63)

---
+++ app/domain/export/pdf/invoice/header_without_address.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2012-2026, Schweizer Wanderwege. This file is part of
+#  hitobito_sww and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sww.
+
+class Export::Pdf::Invoice::HeaderWithoutAddress < Export::Pdf::Invoice::Header
+  def render_address
+    # don't render address
+  end
+end
