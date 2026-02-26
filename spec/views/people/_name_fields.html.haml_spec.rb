86ea53fe3aec59566d082eed34aa922d2de4dc43
Daniel Illi <daniel@illi.zone>
2026-02-26 21:10:25 +0100

    Do not allow users to edit their own names/email (#307)

---
+++ spec/views/people/_name_fields.html.haml_spec.rb
@@ -0,0 +1,47 @@
+#  Copyright (c) 2026, Schweizer Wanderwege. This file is part of
+#  hitobito_sww and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sww
+
+require "spec_helper"
+
+describe "people/_name_fields.html.haml" do
+  let(:person) { people(:berner_wanderer) }
+  let(:form_builder) { StandardFormBuilder.new(:person, person, view, {}) }
+
+  before do
+    allow(view).to receive(:entry).and_return(person)
+    allow(controller).to receive(:permitted_attrs).and_return(permitted_attrs)
+  end
+
+  subject do
+    render partial: "people/name_fields", locals: {f: form_builder}
+    Capybara::Node::Simple.new(@rendered)
+  end
+
+  context "when attributes are permitted" do
+    let(:permitted_attrs) { [:first_name, :last_name, :nickname] }
+
+    it "renders first_name as input field" do
+      is_expected.to have_css("input[name='person[first_name]']")
+    end
+
+    it "renders last_name as input field" do
+      is_expected.to have_css("input[name='person[last_name]']")
+    end
+  end
+
+  context "when attributes are not permitted" do
+    let(:permitted_attrs) { [:other] }
+
+    it "renders first_name as plaintext" do
+      is_expected.to have_css("p.form-control-plaintext", text: person.first_name)
+      is_expected.not_to have_css("input[name='person[first_name]']")
+    end
+
+    it "renders last_name as plaintext" do
+      is_expected.to have_css("p.form-control-plaintext", text: person.last_name)
+      is_expected.not_to have_css("input[name='person[last_name]']")
+    end
+  end
+end
