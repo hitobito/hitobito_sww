c94d52a43f52313982a0c548fb69ea2b04b823ad
carlobeltrame <carlo@beltra.me>
2026-07-31 16:39:32 +0200

    Match UI permissions when reading event participant attributes in the API (hitobito/hitobito_pbs#466)

--- app/resources/sww/person_resource.rb
+++ app/resources/sww/person_resource.rb
@@ -10,13 +10,19 @@ module Sww::PersonResource
 
   included do
     attribute :title, :string
-    attribute :magazin_abo_number, :integer
-    attribute :sww_cms_profile_id, :integer, writable: false
+    attribute :magazin_abo_number, :integer, readable: :show_details_on_person?
+    attribute :sww_cms_profile_id, :integer, writable: false,
+      readable: :show_details_on_person?
     attribute :updated_at, :datetime, writable: false
 
     belongs_to :updated_by,
       foreign_key: :updater_id,
       resource: PersonResource,
       writable: false
+  end
+
+  # For attributes that are not viewable in the UI on an event participation of this person
+  def show_details_on_person?(model_instance)
+    can?(:show_details, model_instance)
   end
 end
